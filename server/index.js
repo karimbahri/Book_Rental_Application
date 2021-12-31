a28434eae6b01bd96f34bc11cedc80ec8c229b96
karim <karim-bahri-18@outlook.fr>
2021-12-31 15:34:13 +0100

    code organization

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+const db_connect = require('./database/connections');
+const routes = require('./routes/routes');
+
+dotenv.config({path: "./config.env"});
+
+const PORT = process.env.PORT || 5000;
+
+db_connect();
+
+/* MIDDLEWARES */
+app.use(express.json());
+app.use('/', routes);
+app.use((err, req, res, next) => {
+    res
+    .status(504)
+    .json({
+        status: "Failure",
+        message: err.message || "Server error"
+    });
+    next();
+})
+/* MIDDLEWARES */
+
+
+app.listen(5000, () => {
+    console.log(`server running on LocalHost:${PORT}`);
+})
