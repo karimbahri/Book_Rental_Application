b4493083aa354ae7f3988a6b8dd6a85f5cfb6513
Moussaddak <meddeb.moussaddak@gmail.com>
2022-01-11 01:51:52 +0100

    invite user ok

--- server/controllers/mailRegistrationControllers.js
+++ server/controllers/mailRegistrationControllers.js
@@ -1,44 +1,42 @@
 const nodemailer = require("nodemailer");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 exports.addUser = async (req, res) => {
-    if (!req.body) {
-        return res
-        .status(403)
-        .json({
-            status: 'Failure',
-            message: 'Cannot post empty object'
-        })
-    }
-    try {
-        let transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: "med.hedi.marrakchi@gmail.com", // generated ethereal user
-              pass: "Mm)`hvW7sPn\\>72]", // generated ethereal password
-            },
-          });
-    
-        let info = await transporter.sendMail({
-            from: 'Book rental',
-            to: req.body.email,
-            subject: "Invitation",
-            text: `http://localhost:3000/registration/${uuidv4()}`,
-            // html: "<b></b>",
-          });
-        res
-        .status(202)
-        .json({
-            status: 'Success'
-        })
-    }
-    catch(err) {
-        res
-        .status(406)
-        .json({
-            status: 'Failure',
-            message: err.message || "cannot send email"
-        })
-    }
+  if (!req.body) {
+    return res.status(403).json({
+      status: "Failure",
+      message: "Cannot post empty object",
+    });
+  }
+  try {
+    console.log(req.body);
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "med.hedi.marrakchi@gmail.com", // generated ethereal user
+        pass: "Mm)`hvW7sPn\\>72]", // generated ethereal password
+      },
+    });
 
-}
+    let info = await transporter.sendMail({
+      from: "Book rental",
+      to: req.body.email,
+      subject: "Invitation",
+      html: `<h2>Hey and welcome</h2>
+            <div>
+                <b>I'm so happy to have you as part of our community!</b> 🥳<br>
+                <b>Please feel free to proceed with your inscription by clicking the link below.</b><br>
+            <a href='http://localhost:3000/registration/${uuidv4()}'>create account now</a><br><br>
+            All the best,
+            </div>`,
+    });
+    res.status(202).json({
+      status: "Success",
+    });
+  } catch (err) {
+    res.status(406).json({
+      status: "Failure",
+      message: err.message || "cannot send email",
+    });
+  }
+};
